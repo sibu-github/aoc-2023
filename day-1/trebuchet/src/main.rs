e172b7e2ad8530825002f98567306edbb0da7349
Sibaprasad Maiti <sibu.it13@gmail.com>
2023-12-02 00:01:45 +0530

    day 1 solution

--- day-1/trebuchet/src/main.rs
+++ day-1/trebuchet/src/main.rs
@@ -3,71 +3,45 @@ use std::{convert::Infallible, iter::Sum, str::FromStr};
 #[derive(Debug)]
 struct Num(u32);
 
-fn check_pattern(s: &str, idx: usize) -> Option<char> {
+fn check_pattern(s: &str, idx: usize) -> Option<u32> {
     let s = &s[idx..];
-    if s.starts_with(P_ONE.0) || s.starts_with(P_ONE.1) {
-        Some('1')
-    } else if s.starts_with(P_TWO.0) || s.starts_with(P_TWO.1) {
-        Some('2')
-    } else if s.starts_with(P_THREE.0) || s.starts_with(P_THREE.1) {
-        Some('3')
-    } else if s.starts_with(P_FOUR.0) || s.starts_with(P_FOUR.1) {
-        Some('4')
-    } else if s.starts_with(P_FIVE.0) || s.starts_with(P_FIVE.1) {
-        Some('5')
-    } else if s.starts_with(P_SIX.0) || s.starts_with(P_SIX.1) {
-        Some('6')
-    } else if s.starts_with(P_SEVEN.0) || s.starts_with(P_SEVEN.1) {
-        Some('7')
-    } else if s.starts_with(P_EIGHT.0) || s.starts_with(P_EIGHT.1) {
-        Some('8')
-    } else if s.starts_with(P_NINE.0) || s.starts_with(P_NINE.1) {
-        Some('9')
-    } else {
-        None
-    }
+    ALL_PATTERNS
+        .iter()
+        .find(|p| s.starts_with(p.0) || s.starts_with(p.1))
+        .map(|p| p.2)
 }
 
-const P_ONE: (&str, &str) = ("one", "1");
-const P_TWO: (&str, &str) = ("two", "2");
-const P_THREE: (&str, &str) = ("three", "3");
-const P_FOUR: (&str, &str) = ("four", "4");
-const P_FIVE: (&str, &str) = ("five", "5");
-const P_SIX: (&str, &str) = ("six", "6");
-const P_SEVEN: (&str, &str) = ("seven", "7");
-const P_EIGHT: (&str, &str) = ("eight", "8");
-const P_NINE: (&str, &str) = ("nine", "9");
+type PATTERN<'a> = (&'a str, &'a str, u32);
+const P_ONE: PATTERN = ("one", "1", 1);
+const P_TWO: PATTERN = ("two", "2", 2);
+const P_THREE: PATTERN = ("three", "3", 3);
+const P_FOUR: PATTERN = ("four", "4", 4);
+const P_FIVE: PATTERN = ("five", "5", 5);
+const P_SIX: PATTERN = ("six", "6", 6);
+const P_SEVEN: PATTERN = ("seven", "7", 7);
+const P_EIGHT: PATTERN = ("eight", "8", 8);
+const P_NINE: PATTERN = ("nine", "9", 9);
+const ALL_PATTERNS: [PATTERN; 9] = [
+    P_ONE, P_TWO, P_THREE, P_FOUR, P_FIVE, P_SIX, P_SEVEN, P_EIGHT, P_NINE,
+];
 
 impl FromStr for Num {
     type Err = Infallible;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut first = '0';
-        let mut last = '0';
-        let mut idx = 0;
-        loop {
-            if idx >= s.len() {
+        let mut first = 0;
+        let mut last = 0;
+        for idx in 0..s.len() {
+            if let Some(n) = check_pattern(s, idx) {
+                first = n;
                 break;
-            }
-            if let Some(ch) = check_pattern(s, idx) {
-                first = ch;
-                break;
-            }
-            idx += 1;
-        }
-        let mut idx = s.len() - 1;
-        loop {
-            if let Some(ch) = check_pattern(s, idx) {
-                last = ch;
-                break;
-            }
-            if idx == 0 {
-                break;
-            } else {
-                idx -= 1;
             }
         }
-        let first = first.to_digit(10).unwrap();
-        let last = last.to_digit(10).unwrap();
+        for idx in (0..s.len()).rev() {
+            if let Some(n) = check_pattern(s, idx) {
+                last = n;
+                break;
+            }
+        }
         let num = first * 10 + last;
         Ok(Self(num))
     }
